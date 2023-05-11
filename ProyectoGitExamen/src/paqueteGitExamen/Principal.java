b8980e564d240ac80ed6c85f6e4610a2817f3c53
DAM1 <carmona33lol@gmail.com>
2023-05-11 20:08:50 +0200

    Este es el mensaje del commit 1

---
+++ ProyectoGitExamen/src/paqueteGitExamen/Principal.java
@@ -0,0 +1,10 @@
+package paqueteGitExamen;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Fran, no me suspendas porfa");
+	}
+
+}

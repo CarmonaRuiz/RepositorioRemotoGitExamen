9289d42c061bc555a6aaddc04be4129a6c70bc9c
DAM1 <carmona33lol@gmail.com>
2023-05-11 20:18:39 +0200

    Mensajes 2

---
+++ ProyectoGitExamen/src/paquete2/Principal.java
@@ -0,0 +1,11 @@
+package paquete2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto son 2 nuevos mensajes");
+		System.out.println("Por ahora va perfecto verdad, Fran?");
+	}
+
+}
